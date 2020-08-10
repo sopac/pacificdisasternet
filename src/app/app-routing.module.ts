c5f0909bd64e8fb8b7efd1af17a5f170feb00e77
Sachindra Singh <sachindras@spc.int>
2020-08-10 12:10:20 +1200

    Pre-Miration to Angular 10x

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DisasterComponent } from './disaster/disaster.component';
+import { ProjectComponent } from './project/project.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'pdalo', component: DisasterComponent },
+  { path: 'projects', component: ProjectComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,44 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatTableModule } from '@angular/material/table';
+
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+
+import { DisasterComponent } from './disaster/disaster.component';
+import { ProjectComponent } from './project/project.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DisasterComponent,
+    ProjectComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    NgxChartsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatIconModule,
+    MatDividerModule,
+    MatBadgeModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
